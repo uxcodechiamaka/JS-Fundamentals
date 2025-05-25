b1beb0684b16a2eadcd5156c62331827fc2f6451
uxcodechiamaka <chiamakanwankwo557@gmail.com>
2025-05-25 22:42:46 +0000

    task 6

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+let myArrary = ["C is fun", "Python is cool", "JavaScript is amazing"]
+
+for (eachArray of myArrary){
+    console.log(eachArray)
+}
--- test.js
+++ test.js
@@ -1,3 +1,14 @@
-const name = Number("Abc")
+/*let fruit0 = "Orange"
+let fruit1 = "Apple"
+let fruit2 = "Mango"
 
-console.log(name)
+console.log(fruit0)
+console.log(fruit1)
+console.log(fruit2)*/
+
+
+let fruits = ["Orange", "Apple", "Mango"]
+
+for (fruit of fruits){
+    console.log(fruit)
+}
